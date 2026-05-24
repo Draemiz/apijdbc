6172b8fe0d90f836aa7e156dd8b606b3b369d08f
Draemiz <draemizgx1@gmail.com>
2026-05-23 19:00:52 -0500

    Proyecto API REST JDBC para pruebas funcionando correctamente

--- src/main/java/com/krakdev/apijdbc/ApijdbcApplication.java
+++ src/main/java/com/krakdev/apijdbc/ApijdbcApplication.java
@@ -2,8 +2,10 @@ package com.krakdev.apijdbc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.krakdev")
 public class ApijdbcApplication {
 
 	public static void main(String[] args) {
